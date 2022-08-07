f1485b04da1fbbbffbfc98c404fbce466a1b7522
David Jeyachandran <david.jeyachandran@sonder.io>
2022-08-08 09:05:10 +1000

    Add files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "todoamplify": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+
 
 function App() {
   return (
